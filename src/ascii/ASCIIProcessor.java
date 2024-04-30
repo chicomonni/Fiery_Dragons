797815abc68ab0ddff752b23d3bd9735b02ec8c6
Audrey Phommasone <apho0008@student.monash.edu>
2024-04-30 23:34:16 +1000

    - Separated ASCII art into individual text files - Added ASCII processor to create HTML outputs for each text file

---
+++ src/ascii/ASCIIProcessor.java
@@ -0,0 +1,35 @@
+package ascii;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ASCIIProcessor {    
+    public static String getArt(String filename) throws FileNotFoundException {
+        File asciiArt = new File(filename);
+        Scanner scanner = new Scanner(asciiArt);
+        try {            
+            StringBuilder string = new StringBuilder();
+
+            while (scanner.hasNextLine()) {
+                string.append(scanner.nextLine());
+                string.append("<br>");
+            }
+            scanner.close();
+            string.insert(0, "<html><pre style=\"font-family: MxPlus IBM BIOS\">");
+            string.append("</pre></html>");
+            return string.toString();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getArtWithValue(String filename, int value) throws FileNotFoundException {
+        String string = getArt(filename);
+        String.format("My answer is %15.8f",
+                                    47.65734);
+ 
+        return string.toString().replaceAll("xx", String.format("%02d",value));
+    }
+}
