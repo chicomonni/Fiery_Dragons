19213ce513efb48971292f60009461f07330de37
Audrey Phommasone <apho0008@student.monash.edu>
2024-04-30 20:26:56 +1000

    Added buttons for dragonCard selections

--- src/Display.java
+++ src/Display.java
@@ -11,6 +11,8 @@ class Display {
     private JFrame frame;
     private JLabel label;
     private JButton nextPlayerButton;
+    private JPanel buttonPanel;
+    private JPanel cardPanel;
     private ArrayList<JButton> cardButtons = new ArrayList<>();
 
     Display(String gameName) throws IOException, FontFormatException {
@@ -26,29 +28,39 @@ class Display {
         frame = new JFrame(gameName);
         label = new JLabel("",SwingConstants.CENTER);
         label.setFont(new Font("MxPlus IBM BIOS", Font.PLAIN, 8));
-        nextPlayerButton = new JButton("End Turn");
-
+        
+        GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(Color.BLACK);
         frame.setSize(1200, 800);
         frame.setResizable(false);
         frame.setVisible(true);
 
-        label.setForeground(Color.WHITE);
-        label.setSize(1200, 800);
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(20,20,20,20);
 
+        label.setForeground(Color.WHITE);
+        label.setSize(1200, 600);
+
+        frame.setLayout(new GridBagLayout());
+        frame.add(label, c);
+
+        nextPlayerButton = new JButton("End Turn");
         nextPlayerButton.setPreferredSize(new Dimension(150, 41));
         nextPlayerButton.setLocation(800, 800);
+        
+        cardPanel = new JPanel();
+        cardPanel.setLayout(new GridLayout(5,4));
+        cardPanel.setBackground(Color.BLACK);
+        frame.add(cardPanel, c);
 
-        GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
-        frame.add(label);
-        frame.add(nextPlayerButton);
-        frame.setLayout(new FlowLayout());
+        ArrayList<String> string = new ArrayList<String>();
+        string.add("this is a dragoncard");
 
-        // ArrayList<String> string = new ArrayList<String>();
-        // string.add("Card 1");
-        // string.add("Card 2");
-        // addCardButton(string);
+        for (int i = 0; i < 16; i++) {
+            addCardButton(string);
+        }
+        cardPanel.add(nextPlayerButton, c);
     }
 
     public void addCardButton(ArrayList<String> cardDisplay) {
@@ -60,10 +72,9 @@ class Display {
         string.insert(0, "<html>");
         string.append("</html>");
         JButton cardButton = new JButton(string.toString());
-        cardButton.setPreferredSize(new Dimension(150, 200));
+        cardButton.setPreferredSize(new Dimension(150, 300));
         cardButton.setLocation(800, 800);
         cardButtons.add(cardButton);
-        frame.add(cardButton);
-        cardButton.setEnabled(false);
+        cardPanel.add(cardButton);
     }
 }
