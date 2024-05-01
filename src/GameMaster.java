5235bd75f87098107f18f6bf1ca8fbdcdd62a10f
gkan0011 <gkan0011@student.monash.edu>
2024-05-01 21:22:04 +1000

    commenting

--- src/GameMaster.java
+++ src/GameMaster.java
@@ -4,6 +4,8 @@
  * @author Georgia Kanellis
  */
 public class GameMaster {
+    //GameBoard print not working properly if numChit does not divide equally by maxCardPerRow
+    //throws an error
     private static final int numChitCards = 16; //default number of chit cards
     private static final int numVolcanoTiles = 24; //default
     private static final int numPlayers = 4; //default
