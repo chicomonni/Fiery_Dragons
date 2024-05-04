a09c59040378c8996a42f8ab8d0337f7894416f6
tyesamuels <tsam0016@student.monash.edu>
2024-05-05 02:16:14 +1000

    created concrete ChitStrategy implementations

---
+++ src/chits/strategies/PirateChitStrategy.java
@@ -0,0 +1,32 @@
+package chits.strategies;
+
+import chits.Chit;
+
+/**
+ * Concrete strategy class for a pirate Chit. Game rule: pirate Chits move the Player backwards no matter the Chit
+ * they're standing on
+ */
+public class PirateChitStrategy implements ChitStrategy {
+    /**
+     * Used to check if Chit on card matches Chit on Volcano
+     *
+     * @param self  the Chit calling the method
+     * @param other the other Chit being compared
+     * @return {@code true} if Chits are compatible, {@code false} otherwise
+     */
+    @Override
+    public boolean validate(Chit self, Chit other) {
+        return true;
+    }
+
+    /**
+     * Used to modify value of ChitCard (e.g. negate the value to move backwards)
+     *
+     * @param value the value to modify
+     * @return the modified value
+     */
+    @Override
+    public int modifyValue(int value) {
+        return -value;
+    }
+}
