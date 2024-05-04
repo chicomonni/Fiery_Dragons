3bb479232cb1cec37dc6576bb36ea47c45c6c2b5
tyesamuels <tsam0016@student.monash.edu>
2024-05-05 01:20:31 +1000

    Created jar

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main class of program
+ */
+public class Main {
+    /**
+     * Method that is run on startup of jar
+     */
+    public static void main(String[] args) {
+//        new FieryDragons().start();
+    }
+}
