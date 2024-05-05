bcfb1ffeca18111a22f1e4ed2023d95e879edae5
tyesamuels <tsam0016@student.monash.edu>
2024-05-05 10:54:39 +1000

    Implemented DefaultSquareMoveStrategy

---
+++ src/game/tiles/strategies/DefaultSquareMoveStrategy.java
@@ -0,0 +1,43 @@
+package game.tiles.strategies;
+
+import game.MoveAction;
+import game.Player;
+import game.tiles.Square;
+
+/**
+ * The default implementation of move for a Square not connected to a Cave
+ */
+public class DefaultSquareMoveStrategy implements SquareMoveStrategy {
+    private final Square square;
+
+    /**
+     * Constructor
+     *
+     * @param square the Square instance that uses this strategy
+     */
+    public DefaultSquareMoveStrategy(Square square) {
+        this.square = square;
+    }
+
+    /**
+     * Returns the desired MoveAction if it can be performed
+     *
+     * @param player the Player instance moving
+     * @param dist   how far the Player moves along the Volcano
+     * @return the desired MoveAction or {@code null} if it cannot be performed
+     */
+    @Override
+    public MoveAction move(Player player, int dist) {
+        if (!square.canEnter(player)) {
+            return null;
+        }
+
+        if (dist == 0) {
+            return new MoveAction();
+        } else if (dist > 0) {
+            return square.getNext().move(player, dist - 1);
+        } else {
+            return square.getPrev().move(player, dist + 1);
+        }
+    }
+}
