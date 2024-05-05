a87e6cdd8fb7529ba7933683180634bb338b4e42
tyesamuels <tsam0016@student.monash.edu>
2024-05-05 22:22:46 +1000

    added method to create ChitCards

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,10 +1,12 @@
 package game;
 
+import game.chitCards.ChitCardArray;
 import game.chits.ChitFactory;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.KeyException;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -14,6 +16,7 @@ import java.util.stream.Collectors;
  */
 public class Board {
     private final Volcano volcano = new Volcano();
+    private final ChitCardArray chitCards = new ChitCardArray();
     private final List<String> boardRep;
 
     /**
@@ -31,6 +34,14 @@ public class Board {
         volcano.createVolcano(squareSrc, caveSrc, factory);
     }
 
+    private void createChitCards(String cardSrc, ChitFactory factory) {
+        try {
+            chitCards.createCards(cardSrc, factory);
+        } catch (KeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     /**
      * Method to create Board
      *
@@ -41,6 +52,15 @@ public class Board {
      */
     public void createBoard(String squareSrc, String caveSrc, String cardSrc, ChitFactory factory) {
         createVolcano(squareSrc, caveSrc, factory);
+        createChitCards(cardSrc, factory);
+    }
+
+    public int getNoCaves() {
+        return volcano.getNoCaves();
+    }
+
+    public int getNoSquares() {
+        return volcano.getNoSquares();
     }
 
     public List<String> getBoardRep() {
