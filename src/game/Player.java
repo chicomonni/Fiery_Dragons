e574746c2c14206220fac08307382245feb6eec9
tyesamuels <tsam0016@student.monash.edu>
2024-05-05 22:28:45 +1000

    added attributes to Player

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,6 +1,24 @@
 package game;
 
+import game.tiles.GameTile;
+
+import java.awt.*;
+import java.util.Random;
+
 public class Player {
-    //    TODO: implement
+    public final Color colour = new Color(Color.HSBtoRGB(new Random().nextInt(360), 100, 50));
+    public final char displayChar = '@';
+    private final String name;
+    public GameTile position;
     private Player nextPlayer;
+
+    public Player(String name, GameTile position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    private void executeAction(Board board) {
+        int cardChoice = new Random().nextInt(1, 17);
+        System.out.println("Card chosen was number " + cardChoice);
+    }
 }
