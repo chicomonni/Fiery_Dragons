9e50dae11489bba431563a82a0dee4c59dcd76b3
tyesamuels <tsam0016@student.monash.edu>
2024-05-05 22:27:48 +1000

    added createBoard method

--- src/game/displays/DisplayManager.java
+++ src/game/displays/DisplayManager.java
@@ -1,12 +1,15 @@
 package game.displays;
 
 import game.Board;
+import game.Player;
 
 /**
  * A Facade for interacting with the game Displays and Window
  */
 public class DisplayManager {
     private final GameWindow gameWindow;
+    private VolcanoDisplay volcanoDisplay;
+    private PlayerDisplay playerDisplay;
 
     /**
      * Constructor
@@ -18,6 +21,11 @@ public class DisplayManager {
     }
 
     public void createBoard(Board board) {
+        volcanoDisplay = new VolcanoDisplay(board.getBoardRep(), gameWindow);
+        playerDisplay = new PlayerDisplay(board.getNoSquares(), board.getNoCaves());
+    }
 
+    public void displayMove(Player player) {
+//        TODO: implement
     }
 }
