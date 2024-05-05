56cb08a547a1ffef9578ceb79755a763803fa8e2
tyesamuels <tsam0016@student.monash.edu>
2024-05-05 22:28:10 +1000

    added createBoard method

--- src/game/FieryDragons.java
+++ src/game/FieryDragons.java
@@ -12,16 +12,18 @@ import java.io.IOException;
  * Class responsible for initiating the game
  */
 public class FieryDragons {
-//    TODO: implement these classes
-//    private Board board;
-//    private Player player1;
+    public static final DisplayManager display = new DisplayManager(new GameWindow());
+    private static final String SQUARE_SRC = "S0w*SSw**w0*0S0*wS0w0S*w";
+    private static final String CAVE_SRC = "S*w0";
+    private static final String CARD_SRC = "S1S2S3w1w2w3*1*2*3010203P1P1P2P2";
+    private final ChitFactory chitFactory = new ChitFactory();
+    private Board board;
+    private Player player1;
 
     /**
      * Method for initialising all the Chits in the game
      */
     private void createChits() {
-        ChitFactory chitFactory = new ChitFactory();
-
         try {
             chitFactory.setChit('w', "/assets/CardBat.txt", new AnimalChitStrategy());
             chitFactory.setChit('0', "/assets/CardDragon.txt", new AnimalChitStrategy());
@@ -33,11 +35,17 @@ public class FieryDragons {
         }
     }
 
+    private void createBoard() {
+        board = new Board("/assets/Board.txt");
+        board.createBoard(SQUARE_SRC, CAVE_SRC, CARD_SRC, chitFactory);
+    }
+
     /**
      * This method should be called to start a new game of Fiery Dragons
      */
     public void start() {
         createChits();
-        new DisplayManager(new GameWindow(1050, 800));
+        createBoard();
+        display.createBoard(board);
     }
 }
