934e500ebb0ee3ddb236f28f4125831a3e375f20
tyesamuels <tsam0016@student.monash.edu>
2024-05-16 15:24:40 +1000

    Adjusted the layout of components on the GameWindow

--- src/game/displays/GameWindow.java
+++ src/game/displays/GameWindow.java
@@ -5,17 +5,19 @@ import java.awt.*;
 import java.io.IOException;
 import java.util.Objects;
 
+import static java.lang.Math.ceil;
+
 /**
  * The Fiery Dragon game window
  */
 public class GameWindow {
     private static final String GAME_NAME = "Fiery Dragons";
     private static final String FONT_PATH = "/MxPlus_IBM_BIOS.ttf";
-    private static final int width = 1050;
-    private static final int height = 800;
+    private static final float FONT_SIZE = 5f;
     final JLayeredPane volcano = new JLayeredPane();
     final JPanel chitCards = new JPanel();
     final JPanel input = new JPanel();
+    final JPanel header = new JPanel();
     private final JFrame window = new JFrame(GAME_NAME);
 
     /**
@@ -41,14 +43,9 @@ public class GameWindow {
                 Objects.requireNonNull(getClass().getResourceAsStream(FONT_PATH))
         );
 
-        UIManager.put("Label.font", font.deriveFont(6.75f));
-        UIManager.put("TextPane.font", font.deriveFont(6.75f));
-        UIManager.put("TextArea.font", font.deriveFont(6.75f));
-
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setSize(width, height);
-        window.setResizable(false);
-        window.setLocationRelativeTo(null);
+        UIManager.put("Label.font", font.deriveFont(FONT_SIZE));
+        UIManager.put("TextPane.font", font.deriveFont(FONT_SIZE));
+        UIManager.put("TextArea.font", font.deriveFont(FONT_SIZE));
 
         Container container = window.getContentPane();
         container.setLayout(new GridBagLayout());
@@ -56,29 +53,46 @@ public class GameWindow {
 
         GridBagConstraints constraints = new GridBagConstraints();
 
+        constraints.insets = new Insets(5, 10, 5, 5);
         constraints.fill = GridBagConstraints.BOTH;
-        constraints.gridx = 1;
-        constraints.gridy = 0;
-        constraints.weightx = 400;
-        constraints.weighty = 650;
-
-        chitCards.setOpaque(false);
-        container.add(chitCards, constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.weightx = 650;
-
-        container.add(volcano, constraints);
-
-        constraints.gridwidth = GridBagConstraints.REMAINDER;
         constraints.gridx = 0;
         constraints.gridy = 1;
-        constraints.weighty = 150;
 
-        input.setOpaque(false);
+        volcano.setPreferredSize(new Dimension((int) (107 * FONT_SIZE), (int) (107 * (FONT_SIZE + 1))));
+        volcano.setBackground(Color.RED);
+        volcano.setOpaque(true);
+        container.add(volcano, constraints);
+
+        constraints.insets = new Insets(5, 5, 5, 10);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+
+//        chitCards.setOpaque(false);
+        chitCards.setPreferredSize(new Dimension((int) ceil(53 * FONT_SIZE), 20));
+        chitCards.setBackground(Color.BLUE);
+        container.add(chitCards, constraints);
+
+        constraints.insets = new Insets(5, 10, 10, 10);
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+
+//        input.setOpaque(false);
+        input.setBackground(Color.YELLOW);
+        input.setPreferredSize(new Dimension(32, 32));
         container.add(input, constraints);
 
+        constraints.insets = new Insets(10, 10, 5, 10);
+        constraints.gridy = 0;
+
+        header.setBackground(Color.YELLOW);
+        header.setPreferredSize(new Dimension(32, 32));
+        container.add(header, constraints);
+
+        window.pack();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
 }
