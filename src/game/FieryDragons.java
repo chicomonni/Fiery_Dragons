0469bd10b377d75cf1f834f97f00b2d47f15f6bc
tyesamuels <tsam0016@student.monash.edu>
2024-05-17 00:32:35 +1000

    Created players

--- src/game/FieryDragons.java
+++ src/game/FieryDragons.java
@@ -5,8 +5,10 @@ import game.chits.strategies.AnimalChitStrategy;
 import game.chits.strategies.PirateChitStrategy;
 import game.displays.DisplayManager;
 import game.displays.GameWindow;
+import game.tiles.Cave;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Class responsible for initiating the game
@@ -15,6 +17,7 @@ public class FieryDragons {
     private static final String SQUARE_SRC = "S0w*SSw**w0*0S0*wS0w0S*w";
     private static final String CAVE_SRC = "S*w0";
     private static final String CARD_SRC = "S1,S2,S3,w1,w2,w3,*1,*2,*3,01,02,03,P1,P1,P2,P2";
+    private static final int NUM_PLAYERS = 4;
     private final DisplayManager display = new DisplayManager(new GameWindow());
     private final ChitFactory chitFactory = new ChitFactory();
     private Board board;
@@ -40,12 +43,30 @@ public class FieryDragons {
         board.createBoard(SQUARE_SRC, CAVE_SRC, CARD_SRC, chitFactory);
     }
 
+    private void createPlayers() {
+        Cave[] caves = board.getCaves();
+        Player[] players = new Player[NUM_PLAYERS];
+        assert players.length <= caves.length : "Not enough caves for all players";
+
+        for (int i = 0; i < players.length; i++) {
+            players[i] = new Player("Player " + (i + 1), Objects.requireNonNull(caves[0]));
+            caves[0].setResident(players[i]);
+        }
+
+        for (int i = 0; i < players.length; i++) {
+            players[i].setNextPlayer(players[(i + 1) % players.length]);
+        }
+
+        player1 = players[0];
+    }
+
     /**
      * This method should be called to start a new game of Fiery Dragons
      */
     public void start() {
         createChits();
         createBoard();
+        createPlayers();
         display.createBoard(board);
     }
 }
