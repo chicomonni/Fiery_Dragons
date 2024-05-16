eb534c0e83558a2bd3c09985b26422d2c3ace814
tyesamuels <tsam0016@student.monash.edu>
2024-05-16 23:59:15 +1000

    added getter and setter for position

--- src/game/Player.java
+++ src/game/Player.java
@@ -9,7 +9,7 @@ public class Player {
     public final Color colour = new Color(Color.HSBtoRGB(new Random().nextInt(360), 100, 50));
     public final char displayChar = '@';
     private final String name;
-    public GameTile position;
+    private GameTile position;
     private Player nextPlayer;
 
     public Player(String name, GameTile position) {
@@ -20,5 +20,23 @@ public class Player {
     private void executeAction(Board board) {
         int cardChoice = new Random().nextInt(1, 17);
         System.out.println("Card chosen was number " + cardChoice);
+    }
+
+    /**
+     * Get this Player's current position
+     *
+     * @return GameTile currently occupied by this Player
+     */
+    public GameTile getPosition() {
+        return position;
+    }
+
+    /**
+     * Set this Player's position
+     *
+     * @param position GameTile the Player now occupies
+     */
+    public void setPosition(GameTile position) {
+        this.position = position;
     }
 }
