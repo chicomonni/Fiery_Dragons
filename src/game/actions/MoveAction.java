1593503837c2777c0a9dba2abbdd017b447d4395
tyesamuels <tsam0016@student.monash.edu>
2024-05-16 23:59:36 +1000

    changed constructor

--- src/game/actions/MoveAction.java
+++ src/game/actions/MoveAction.java
@@ -3,23 +3,20 @@ package game.actions;
 import game.Board;
 import game.Player;
 import game.displays.DisplayManager;
-import game.tiles.GameTile;
 
 public class MoveAction implements Action {
+    private final Player player;
+    private final int dist;
 
-    public final GameTile to;
-    public final Player player;
 
-    public MoveAction(GameTile to, Player player) {
-        this.to = to;
+    public MoveAction(Player player, int dist) {
         this.player = player;
+        this.dist = dist;
     }
 
     @Override
     public void execute(Board board, DisplayManager display) {
-        player.position.occupied(false);
-        to.occupied(true);
-        player.position = to;
+        player.getPosition().move(player, dist);
     }
 
     @Override
