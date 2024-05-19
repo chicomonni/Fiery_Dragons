d4800256d8a63f01b7742a326b0b904a2b09196f
Audrey Phommasone <apho0008@student.monash.edu>
2024-05-19 21:12:49 +1000

    Updated WinAction documentation

--- src/game/actions/WinAction.java
+++ src/game/actions/WinAction.java
@@ -3,12 +3,27 @@ package game.actions;
 import game.Board;
 import game.displays.DisplayManager;
 
+/**
+ * Class used to represent a game action to end the game.
+ */
 public class WinAction implements GameAction {
 
+    /**
+     * Constructor
+     */
+    public WinAction() {
+    }
+
+    /**
+     * Executes the action to end the game.
+     *
+     * @param board   the game board
+     * @param display the display manager for updating the game interface
+     * @return null since this action does not generate any new action
+     */
     @Override
     public GameAction execute(Board board, DisplayManager display) {
         System.out.println("You win!");
         return null;
     }
-    
 }
