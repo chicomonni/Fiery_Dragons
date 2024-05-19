b8d2f45f9f68cab6d147cd2d5f9c20ff5e3baa93
gkan0011 <gkan0011@student.monash.edu>
2024-05-19 20:23:48 +1000

    docstrings

--- src/game/actions/NextTurnGameAction.java
+++ src/game/actions/NextTurnGameAction.java
@@ -4,14 +4,28 @@ import game.Board;
 import game.Player;
 import game.displays.DisplayManager;
 
+/**
+ * Class used to represent a game action to proceed to the next turn.
+ */
 public class NextTurnGameAction implements GameAction {
     private final Player player;
 
+    /**
+     * Constructor
+     *
+     * @param player the player for whom the next turn action is being executed
+     */
     public NextTurnGameAction(Player player) {
         this.player = player;
     }
 
-
+    /**
+     * Executes the action to proceed to the next turn for the player.
+     *
+     * @param board   the game board
+     * @param display the display manager for updating the game interface
+     * @return null since this action does not generate any new action
+     */
     @Override
     public GameAction execute(Board board, DisplayManager display) {
         player.startTurn(board, display);
