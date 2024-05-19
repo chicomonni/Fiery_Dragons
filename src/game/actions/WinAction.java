767e61437ff74390e2b56aa53d5f7d6b43563fc7
Audrey Phommasone <apho0008@student.monash.edu>
2024-05-19 21:10:51 +1000

    Created WinAction

---
+++ src/game/actions/WinAction.java
@@ -0,0 +1,14 @@
+package game.actions;
+
+import game.Board;
+import game.displays.DisplayManager;
+
+public class WinAction implements GameAction {
+
+    @Override
+    public GameAction execute(Board board, DisplayManager display) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
