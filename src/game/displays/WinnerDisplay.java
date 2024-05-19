4d392a7e0024add67bb94578f9db69439c4480f4
Audrey Phommasone <apho0008@student.monash.edu>
2024-05-19 23:45:46 +1000

    Created WinnerDisplay class

---
+++ src/game/displays/WinnerDisplay.java
@@ -0,0 +1,23 @@
+package game.displays;
+
+import javax.swing.JLabel;
+import game.Player;
+
+public class WinnerDisplay {
+    
+    private final JLabel winnerPane;
+    /**
+     * Constructor
+     *
+     * @param volcano    the Volcano instance used by the game
+     * @param gameWindow the GameWindow instance this class affects
+     */
+    public WinnerDisplay(GameWindow gameWindow) {
+        winnerPane = gameWindow.getWinnerDisplay();
+    }
+
+    public void displayWinner(Player player) {
+        winnerPane.setText(player.getName() + " wins!");
+        winnerPane.setForeground(player.getColour());
+    }
+}
